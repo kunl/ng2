e51cbdf3b319e9b2d27af1aa3873d476f8e7b741
kunl <le720@outlook.com>
2016-01-05 23:06:41 +0800

    demo

---
+++ app/component/footer.component.ts
@@ -0,0 +1,17 @@
+/**
+ *
+ *
+ * Created by kunl on 2016/1/5-0005.
+ */
+
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'footer',
+    template: `
+        <p>footer</p>
+    `
+})
+
+export class Footer{}
