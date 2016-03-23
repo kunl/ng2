a5aa4febdee22eea1f1b3a14b6e088f1683c2ded
kun l <kun l>
2016-03-23 17:43:21 +0800

    升级 angular2 到 beta11

--- app/component/footer.component.ts
+++ app/component/footer.component.ts
@@ -1,16 +1,10 @@
-/**
- *
- *
- * Created by kunl on 2016/1/5-0005.
- */
-
 
 import {Component} from 'angular2/core';
 
 @Component({
     selector: 'footer',
     template: `
-        <p>footer</p>
+        <p>footer... </p>
     `
 })
 
